f107b813fee9fc9a65d77505508de076fe680fcd
SachinJadhav222 <sachin.jadhav222@yahoo.com>
2018-03-20 22:38:41 +0000

    addition

--- src/test/java/com/turing/niallbunting/Hooks.java
+++ src/test/java/com/turing/niallbunting/Hooks.java
@@ -1,5 +1,7 @@
 package com.turing.niallbunting;
 
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import io.restassured.RestAssured;
 import org.apache.log4j.Logger;
@@ -7,9 +9,19 @@ import org.apache.log4j.Logger;
 public class Hooks {
     Logger logger=Logger.getLogger("Hooks");
     @Before
-    public void setup(){
+    public void setup(Scenario scenario){
         RestAssured.baseURI = "http://turing.niallbunting.com";
         RestAssured.port = 3001;
+        logger.info(scenario.getName());
         logger.info("Base URL and PORT is up and running");
+    }
+    @After
+    public void afterScenario(Scenario scenario){
+        if (scenario.isFailed()) {
+            logger.info("Scenario Failed ");
+        } else {
+            logger.info("Scenario Passed ");
+        }
+
     }
 }
